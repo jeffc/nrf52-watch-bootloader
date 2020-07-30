8d5181d10e04188d0f1308cfba119042bc11325f
Jeff Cooper <jeff@thejeffcooper.com>
2020-07-29 20:12:05 -0400

    make changes for watch

--- src/boards/watch/board.h
+++ src/boards/watch/board.h
@@ -22,43 +22,46 @@
  * THE SOFTWARE.
  */
 
-#ifndef _ARDUINOBLE33_NRF52840_H
-#define _ARDUINOBLE33_NRF52840_H
+#ifndef WATCH_H
+#define WATCH_H
 
-#define _PINNUM(port, pin)    ((port)*32 + (pin))
+#define _PINNUM(port, pin)      ((port)*32 + (pin))
 
 /*------------------------------------------------------------------*/
 /* LED
  *------------------------------------------------------------------*/
-#define LEDS_NUMBER           1  // red LED
-#define LED_PRIMARY_PIN       _PINNUM(0, 24)
-#define LED_STATE_ON          0
+#define LEDS_NUMBER             1
+#define LED_PRIMARY_PIN         _PINNUM(1, 13)
+#define LED_STATE_ON            0
 
 /*------------------------------------------------------------------*/
 /* BUTTON
  *------------------------------------------------------------------*/
-#define BUTTONS_NUMBER        2
-#define BUTTON_1              _PINNUM(1, 11)  // D2 switch
-#define BUTTON_2              _PINNUM(1, 12)  // D3 switch
-#define BUTTON_PULL           NRF_GPIO_PIN_PULLUP
+#define BUTTONS_NUMBER          2
+
+#define BUTTON_1                _PINNUM(0, 15)
+#define BUTTON_2                _PINNUM(0, 10)
+#define BUTTON_PULL             NRF_GPIO_PIN_PULLUP
 
 //--------------------------------------------------------------------+
 // BLE OTA
 //--------------------------------------------------------------------+
-#define BLEDIS_MANUFACTURER   "Arduino"
-#define BLEDIS_MODEL          "Nano 33 BLE"
+#define BLEDIS_MANUFACTURER     "JDC"
+#define BLEDIS_MODEL            "NRF52-WATCH"
 
 //--------------------------------------------------------------------+
 // USB
 //--------------------------------------------------------------------+
+
+// Shared VID/PID with Feather nRF52840, will be disabled for building in the future
 #define USB_DESC_VID           0x239A
-#define USB_DESC_UF2_PID       0x0063
-#define USB_DESC_CDC_ONLY_PID  0x0064
+#define USB_DESC_UF2_PID       0x0029
+#define USB_DESC_CDC_ONLY_PID  0x0029
 
 //------------- UF2 -------------//
-#define UF2_PRODUCT_NAME   "Arduino Nano 33 BLE"
-#define UF2_VOLUME_LABEL   "NANO33BOOT"
-#define UF2_BOARD_ID       "nRF52840-Nano-33"
-#define UF2_INDEX_URL      "https://www.arduino.cc/"
+#define UF2_PRODUCT_NAME        "NRF52 Watch Bootloader"
+#define UF2_VOLUME_LABEL        "WATCHUPDATE"
+#define UF2_BOARD_ID            "nRF52840-MDBT50Q_RX-verD"
+#define UF2_INDEX_URL           "https://hackaday.io/project/169967-nrf52-smartwatch"
 
-#endif // _ITSY_NRF52840_H
+#endif // WATCH_H
